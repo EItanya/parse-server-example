66673f18f5c8069973ebac78f2a359e76a590a18
Eitan-Yarmush <eyarmush212@gmail.com>
2017-02-22 00:08:23 -0600

    I am playing around with cloud code

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,23 @@
 
-Parse.Cloud.define('hello', function(req, res) {
+Parse.Cloud.define('createStory', function(request, response) {
+  console.log(request.params.story)
+  console.log(request.params.entry)
   res.success('Hi');
 });
+
+// Parse.Cloud.define("createStory", function(request, response) {
+//   var query = new Parse.Query("Review");
+//   query.equalTo("movie", request.params.movie);
+//   query.find({
+//     success: function(results) {
+//       var sum = 0;
+//       for (var i = 0; i < results.length; ++i) {
+//         sum += results[i].get("stars");
+//       }
+//       response.success(sum / results.length);
+//     },
+//     error: function() {
+//       response.error("movie lookup failed");
+//     }
+//   });
+// });
